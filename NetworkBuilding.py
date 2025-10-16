3474c6efbf0e6b940e5d7164ce14a6dff29c48aa
elenado-1997 <59953051+elenado-1997@users.noreply.github.com>
2025-10-15 21:59:12 -0500

    Update NetworkBuilding.py

--- NetworkBuilding.py
+++ NetworkBuilding.py
@@ -24,26 +24,38 @@ def buildNetwork(col_name: str, df):
                 network.add_edge(a, b, layer=col_name)
     return network
 
-ArtistNet = buildNetwork("Artist Display Name", df_sample)
-DateNet = buildNetwork("Object Date", df_sample)
-MediumNet = buildNetwork("Medium",df_sample)
-CultureNet = buildNetwork("Culture",df_sample)
+# build networks for each column/category
+
+colNets = {}
+cols = ["Artist Display Name", "Object Date", "Medium", "Culture","Object Number","Department",
+        "Object Name", "Title", "Period", "Dynasty", "Reign", "Portfolio", "Artist Nationality",
+        "Object Begin Date", "Object End Date", "City", "State", "County", "Country", "Region",
+        "Subregion", "Locale", "Classification", "Tags"]
+
+for col in cols:
+    #remove space in category
+    col_no_space = col.replace(" ", "")
+
+    #build network for all categories in the following format: 
+        # ArtistNet = buildNetwork("Artist Display Name", df_sample)
+    colNets[f"{col_no_space}Net"] = buildNetwork(col, df_sample)
+
+
+
 
 
 mlNetwork = nx.MultiGraph()
-mlNetwork = nx.compose_all([ArtistNet, DateNet, MediumNet, CultureNet])
+mlNetwork = nx.compose_all(list(colNets.values()))
 
+#add node attributes for all selected columns
 for _, row in df_sample.iterrows():
-    mlNetwork.add_node(row['Object Number'],
-                       title=row['Title'],
-                       artist=row['Artist Display Name'],
-                       date=row['Object Date'],
-                       medium=row['Medium'],
-                       culture=row['Culture'])
+    node_id = row["Object Number"]
+    attrs = {col.replace(" ", "_").lower(): row[col] for col in cols if col in df_sample.columns}
+    mlNetwork.add_node(node_id, **attrs)
 
 node, degree = max(mlNetwork.degree(), key=lambda x: x[1])
 
-#test code
+#tester code
 node_id = "C.I.46.28.2"
 print(f"Node: {node_id}")
 print("Attributes:", mlNetwork.nodes[node_id])
