cdf9b8ecbf448af7285bd30923e13a9b5c841fc9
Cynfor Lu <cynfor.lu@me.com>
2025-11-02 11:36:36 -0600

    Updated dataframe to newly cleaned file

--- Algorithm.py
+++ Algorithm.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Load Data
 script_dir = os.path.dirname(os.path.abspath(__file__))
-df = pd.read_csv(os.path.join(script_dir, "MetObjects.csv"), low_memory = False)
+df = pd.read_csv(os.path.join(script_dir, "MetObjects-Cleaned_Columns-csv_v2.csv"), low_memory = False)
 df.columns = df.columns.str.replace(" ", "_")
 
 # Filter relevant columns
